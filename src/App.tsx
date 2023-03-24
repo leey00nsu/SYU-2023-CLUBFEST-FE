45571c888ad702900103e92ee25edf495697d711
leeyoonsu <dbstndla1212@naver.com>
2023-03-24 17:09:27 +0900

    #5 refactor. lazy 개선

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import { Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import LoadingSpinner from "./components/ui/LoadingSpinner";
 
-const LandingPage = lazy(() => import("./pages/LandingPage"));
+// const LandingPage = lazy(() => import("./pages/LandingPage"));
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <div className="w-screen h-screen flex justify-center overflow-y-auto">
       <Suspense fallback={<LoadingSpinner size="lg" full />}>
         <Routes>
-          <Route path="/" Component={LandingPage} />
+          <Route path="/" element={<LandingPage />} />
           {/* <Route path="/home" element={<MainPage />} /> */}
         </Routes>
       </Suspense>
