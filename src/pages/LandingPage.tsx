b2ef45dc48087938a27d7247b106c4e3fdcb2081
leeyoonsu <dbstndla1212@naver.com>
2023-03-24 15:11:44 +0900

    #5 refactor. suspense 개선 및 로딩스피너 추가

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,7 +5,6 @@ import EnterButton from "../components/ui/EnterButton";
 import "./star.css";
 
 const LandingPage = () => {
-  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
   const enterHandler = () => {
     sessionStorage.setItem("isLanding", "false");
@@ -19,37 +18,29 @@ const LandingPage = () => {
     }
   }, []);
 
-  useEffect(() => {
-    setIsLoading(true);
-  }, []);
-
   const stars = new Array(12).fill(1);
 
-  if (!isLoading) {
-    return <></>;
-  } else {
-    return (
-      <Layout bg="main">
-        <div className="night">
-          {stars.map((star, index) => (
-            <div key={index} className="shooting_star"></div>
-          ))}
-        </div>
-        <article
-          id="landing"
-          className="flex flex-col items-center justify-between"
-        >
-          <p className="mb-2 text-base text-white font-PyeongChang">
-            멋쟁이사자처럼 SYU X 동아리연합회 다원
-          </p>
-          <p className="mb-6 text-4xl text-white text-glow font-PyeongChangBold">
-            2023-1 동아리제
-          </p>
-          <EnterButton onClick={enterHandler}>동아리제 구경하기</EnterButton>
-        </article>
-      </Layout>
-    );
-  }
+  return (
+    <Layout bg="main">
+      <div className="night">
+        {stars.map((star, index) => (
+          <div key={index} className="shooting_star"></div>
+        ))}
+      </div>
+      <article
+        id="landing"
+        className="flex flex-col items-center justify-between"
+      >
+        <p className="mb-2 text-base text-white font-PyeongChang">
+          멋쟁이사자처럼 SYU X 동아리연합회 다원
+        </p>
+        <p className="mb-6 text-4xl text-white text-glow font-PyeongChangBold">
+          2023-1 동아리제
+        </p>
+        <EnterButton onClick={enterHandler}>동아리제 구경하기</EnterButton>
+      </article>
+    </Layout>
+  );
 };
 
 export default LandingPage;
