037e5d3d4ffccf489f6c29b88ee336ff3e1aa3c0
leeyoonsu <dbstndla1212@naver.com>
2023-03-24 14:08:14 +0900

    #5 feat. 랜딩 페이지 디자인 반영 레이아웃 및 폰트 적용 별똥별 효과 추가 TODO. Lighthouse 성능개선

--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -2,13 +2,19 @@ import React from "react";
 
 interface LayoutProps {
   children: React.ReactNode;
+  bg: string;
 }
 const Layout = (props: LayoutProps) => {
-  return (
-    <div className=" relative bg-indigo-600 flex flex-col max-w-[600px] w-full h-full justify-center items-center ">
-      {props.children}
-    </div>
-  );
+  let styles =
+    "overflow-hidden relative bg-indigo-600 flex flex-col max-w-[600px] w-full h-full justify-center items-center ";
+  if (props.bg === "main") {
+    styles = `bg-cover bg-[url('/img/main_background.png')] overflow-hidden relative bg-indigo-600 flex flex-col max-w-[600px] w-full h-full justify-center items-center `;
+  }
+  return <div className={styles}>{props.children}</div>;
+};
+
+Layout.defaultProps = {
+  bg: "",
 };
 
 export default Layout;
