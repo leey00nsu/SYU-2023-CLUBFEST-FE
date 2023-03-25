5a7be57716e89b9537778f2852a8fab82339f9a0
leeyoonsu <dbstndla1212@naver.com>
2023-03-26 00:06:38 +0900

    #2 refactor. 동아리소개 디자인 수정 상단 nav 수정 분과 nav 수정 동아리 소개 박스 디자인 수정

--- src/components/ClubListBox.tsx
+++ src/components/ClubListBox.tsx
@@ -7,17 +7,17 @@ interface ClubListBoxProps {
 const ClubListBox = (props: ClubListBoxProps) => {
   const { clubName, clubSection, clubDesc, clubImg } = props.data;
   return (
-    <div className="flex w-full h-32 my-4 bg-white rounded-lg">
-      <div className="flex justify-center items-center w-20 h-20 bg-center bg-cover bg-no-repeat bg-[url('/img/banner.png')]">
-        <p className="text-white">중앙</p>
-      </div>
-      <section className="flex items-center h-full p-2">
-        <div className="w-20 h-20 bg-gray-400 rounded-full "></div>
+    <div className="flex flex-col w-5/12 p-2  bg-white rounded-lg h-36">
+      <section className="flex flex-col justify-center h-full ">
+        <p className="text-xl font-PyeongChangPeaceBold text-clubfest-blue">
+          {clubName}
+        </p>
+        <p className="text-base font-PyeongChang text-clubfest-blue">
+          {clubDesc}
+        </p>
       </section>
-      <section className="flex flex-col justify-center h-full p-2">
-        <p>{clubName}</p>
-        <p>{clubSection}</p>
-        <p>{clubDesc}</p>
+      <section className="flex justify-end h-full ">
+        <div className="w-16 h-16 bg-gray-400 rounded-full "></div>
       </section>
     </div>
   );
--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -2,10 +2,10 @@ const Header = () => {
   return (
     <header className="flex items-center justify-center w-full p-6">
       <div className="flex flex-col items-center">
-        <p className="mb-1 text-3xl text-white font-PyeongChangBold">
+        <p className="mb-1 text-3xl text-white font-PyeongChangPeaceBold">
           2023-1 동아리제
         </p>
-        <p className="text-[13px] text-white font-PyeongChang">
+        <p className="text-[13px] text-white font-PyeongChangPeace">
           멋쟁이사자처럼 SYU X 동아리연합회 다원
         </p>
       </div>
