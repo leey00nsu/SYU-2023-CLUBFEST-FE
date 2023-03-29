711269e0a418197e0d7a68e092db3028fb675aaf
leeyoonsu <dbstndla1212@naver.com>
2023-03-29 18:45:07 +0900

    #2 feat. 동아리 소개 디테일 페이지 예외처리

--- src/components/clubs/Clubs.tsx
+++ src/components/clubs/Clubs.tsx
@@ -2,6 +2,7 @@ import DivisionButton from "./DivisionButton";
 import ClubList from "./ClubList";
 import { useState } from "react";
 import { dummyList } from "../../assets/dummyList";
+import React from "react";
 
 const Clubs = () => {
   const [curDivision, setCurDivision] = useState("문화분과");
@@ -16,9 +17,8 @@ const Clubs = () => {
     <>
       <section className="flex justify-center w-full mb-8">
         {divisions.map((division, index) => (
-          <>
+          <React.Fragment key={index}>
             <DivisionButton
-              key={index}
               division={division}
               activeDivision={curDivision}
               changeDivision={changeDivisionHandler}
@@ -26,7 +26,7 @@ const Clubs = () => {
             {index !== divisions.length - 1 && (
               <div className="mx-4 text-sm text-white font-PyeongChang">|</div>
             )}
-          </>
+          </React.Fragment>
         ))}
       </section>
       <section className="w-full p-4 overflow-y-auto grow ">
