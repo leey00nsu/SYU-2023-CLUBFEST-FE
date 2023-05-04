e3da43910d23a8fa6a463a3d003eb334d7c24e5a
leey00nsu <dbstndla1212@naver.com>
2023-05-04 14:08:06 +0900

    #2 feat. 분과 정렬 추가

--- src/components/about/AboutButton.tsx
+++ src/components/about/AboutButton.tsx
@@ -3,11 +3,16 @@ import { FiArrowRight } from "react-icons/fi";
 
 interface AboutButtonProps {
   children: React.ReactNode;
+  url: string;
 }
 
 const AboutButton = (props: AboutButtonProps) => {
   return (
-    <a className=" w-fit flex justify-center items-center px-4 py-1 xs:text-base text-sm text-clubfest-blue bg-white cursor-pointer font-PyeongChang rounded-2xl">
+    <a
+      href={props.url}
+      target="_blank"
+      className=" w-fit flex justify-center items-center px-4 py-1 xs:text-base text-sm text-clubfest-blue bg-white cursor-pointer font-PyeongChang rounded-2xl"
+    >
       {props.children}
       <FiArrowRight className="ml-1" />
     </a>
--- src/components/clubs/Clubs.tsx
+++ src/components/clubs/Clubs.tsx
@@ -5,10 +5,10 @@ import { clubList } from "../../assets/clubList";
 import React from "react";
 
 const Clubs = () => {
-  const [curDivision, setCurDivision] = useState("문화분과");
+  const [curDivision, setCurDivision] = useState("문화");
 
   // 문화분과,봉사분과,체육분과,학술분과
-  const divisions = ["문화분과", "봉사분과", "체육분과", "학술분과"];
+  const divisions = ["문화", "봉사", "체육", "학술"];
 
   const changeDivisionHandler = (changedDivision: string) => {
     setCurDivision(changedDivision);
@@ -30,7 +30,7 @@ const Clubs = () => {
         ))}
       </section>
       <section className="w-full p-4 overflow-y-auto grow ">
-        <ClubList sortBy="rank" list={clubList} />
+        <ClubList sortBy="rank" list={clubList} activeDivision={curDivision} />
       </section>
     </>
   );
