532f584c22e2fc9d481a4997a288d23811563531
leey00nsu <dbstndla1212@naver.com>
2023-05-13 02:40:41 +0900

    #28 feat. pc화면 레이아웃 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, Suspense } from "react";
+import { useState, useEffect, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import ErrorPage from "./pages/ErrorPage";
@@ -7,9 +7,18 @@ import LoadingSpinner from "./components/ui/LoadingSpinner";
 
 // const LandingPage = lazy(() => import("./pages/LandingPage"));
 import LandingPage from "./pages/LandingPage";
+import { motion } from "framer-motion";
 
 function App() {
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [bgColor, setBgColor] = useState("#000000");
   const handleResize = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    setInnerWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
     // signature
     console.log(
       `%c
@@ -25,30 +34,65 @@ function App() {
                                                                                                        `,
       "color:green"
     );
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  };
-
-  useEffect(() => {
     handleResize();
     window.addEventListener("resize", handleResize);
 
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  useEffect(() => {
+    if (innerWidth > 1200) {
+      setBgColor("#0a1240");
+    } else {
+      setBgColor("#000000");
+    }
+  }, [innerWidth]);
+
+  let styles = "w-screen flex justify-center overflow-y-auto";
+
+  if (innerWidth > 1200) {
+    styles = "w-screen flex justify-evenly overflow-y-auto";
+  }
+
   return (
-    <div id="app" className="w-screen flex justify-center overflow-y-auto">
-      <Suspense fallback={<LoadingSpinner size="lg" full />}>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/clubs" element={<MainPage menu="clubs" />} />
-          <Route path="/clubs/:clubId" element={<ClubDetailPage />} />
-          <Route path="/timetable" element={<MainPage menu="timetable" />} />
-          <Route path="/about" element={<MainPage menu="about" />} />
-          <Route path="/*" element={<ErrorPage />} />
-        </Routes>
-      </Suspense>
-    </div>
+    <motion.div
+      key={bgColor}
+      initial={{ backgroundColor: innerWidth > 1200 ? "#ffffff" : "#0a1240" }}
+      animate={{ backgroundColor: innerWidth > 1200 ? "#0a1240" : "#ffffff" }}
+      transition={{
+        duration: 0.2,
+      }}
+    >
+      <div id="app" className={styles}>
+        {innerWidth > 1200 && (
+          <motion.div
+            className=" flex justify-center"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.8,
+              delay: 0.1,
+              ease: [0, 0.71, 0.2, 1.01],
+            }}
+          >
+            <img
+              className="w-[600px] object-scale-down"
+              src="/assets/img/promo.png"
+            />
+          </motion.div>
+        )}
+        <Suspense fallback={<LoadingSpinner size="lg" full />}>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/clubs" element={<MainPage menu="clubs" />} />
+            <Route path="/clubs/:clubId" element={<ClubDetailPage />} />
+            <Route path="/timetable" element={<MainPage menu="timetable" />} />
+            <Route path="/about" element={<MainPage menu="about" />} />
+            <Route path="/*" element={<ErrorPage />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </motion.div>
   );
 }
 
